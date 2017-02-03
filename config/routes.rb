91966bcc47c1d1da19d4c6cc75cbd547f00a56f2
Keyerra King <keyerra.king@gmail.com>
2017-02-03 04:41:39 +0000

    Bootstrap Additions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :winners
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
